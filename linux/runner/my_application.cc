2e49d854adbaae8e46e478eea5fe5a748ed439b2
Paxon <azcoder123@gmail.com>
2026-01-29 11:20:48 +0700

    feat: Update application name to "Firebase Console" across multiple platforms

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "fcmapp");
+    gtk_header_bar_set_title(header_bar, "Firebase Console");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "fcmapp");
+    gtk_window_set_title(window, "Firebase Console");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
